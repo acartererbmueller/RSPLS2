d17a6b951f1971b33cde5208c77d278e84fdfc0a
unknown <carter53202@gmail.com>
2020-04-15 08:04:19 -0500

    added classes and skeleton to the program

---
+++ bigbang/Players.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bigbang
+{
+    class Players
+    {
+
+        //Member Variables (IS A)
+
+        //Constructor
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
---
+++ bigbang/computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bigbang
+{
+    class computer
+    {
+
+
+        //Member Variables (HAS A)
+
+        //Constructor
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
